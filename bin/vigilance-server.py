ff161067f045785ce8572722fbed9692851a7d80
Eric Mountain <eric.mountain@gmail.com>
2019-06-16 16:07:58 +0200

    Trying to deal with endZ expiry
    Signed-off-by: Eric Mountain <eric.mountain@gmail.com>

--- bin/vigilance-server.py
+++ bin/vigilance-server.py
@@ -53,6 +53,7 @@ def latestVigilanceMetrics(gauge=Gauge):
         if result['end'] > now:
             gauge.labels(dept=result['dept'], risk=risks[int(result['risk'])-1], startZ=result['start'], endZ=result['end']).set(int(result['level']))
         else:
+            gauge.labels(dept=result['dept'], risk=risks[int(result['risk'])-1], startZ=result['start'], endZ=result['end']).set(0)
             try:
                 gauge.remove(dept=result['dept'], risk=risks[int(result['risk'])-1], startZ=result['start'], endZ=result['end'])
             except ValueError as e:
@@ -62,6 +63,7 @@ def latestVigilanceMetrics(gauge=Gauge):
                 # Seems we get this if the label set hadn't already been defined, which can happen if
                 # we handle an entry with an early end time before handling lines with end times beyond
                 # now
+                print(f'Warning: incorrect use of gauge.remove(): {e}, setting to zero instead')
                 pass
 
 # Create a metric to track time spent and requests made.
